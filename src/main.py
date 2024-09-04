e39adda8acd68dcf1f088e534eaa23e6777cb4a8
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-09-03 21:50:25 -0300

    Add pubsub watcher and consumer

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # Gmail API Documentation by Google => https://googleapis.github.io/google-api-python-client/docs/dyn/gmail_v1
 from googleapiclient.discovery import Resource
+from google.cloud import pubsub_v1
 
 # TODO Move all this credentials logic to a separated file
 from classfiers import USER_CLASSFIERS, GMAIL_SERVICE, MONGO_DATABASE, CLOUD_STORAGE_CLIENT
@@ -18,9 +19,11 @@ from dotenv import load_dotenv
 
 # Logging
 import logging.config
+import os
 from pathlib import Path
 import json
 import atexit
+from pprint import pprint
 
 from gmail import GmailClassifier
 
@@ -93,6 +96,17 @@ async def run_classfiers(
             )
 
 
+def new_message_callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    """Calback function to handle new messages from Pub/Sub.
+
+    Args:
+        message (PubSub message): New message from Pub/Sub.
+    """
+    # print(type(message))
+    print(f"Received message:")
+    pprint(message.data.decode("utf-8"))
+    message.ack()
+
 if __name__ == "__main__":
     start = pendulum.now()
     setup_logging()
@@ -103,9 +117,34 @@ if __name__ == "__main__":
                 GMAIL_SERVICE, MONGO_DATABASE["classifiers"]))
 
     # After that, we setup the Pub/Sub topic to watch for new messages
+    watcher = GMAIL_SERVICE.users().watch(userId="me", body={
+        "topicName": os.getenv("PUBSUB_TOPIC")
+    }).execute()
+    
+    # pprint(watcher)
 
     # Now, starts to watch for new messages
 
+    with pubsub_v1.SubscriberClient() as subscriber:
+        future = subscriber.subscribe(subscription=os.getenv("PUBSUB_SUBSCRIPTION"), callback=new_message_callback)
+        
+        # FIXME This should stop with CTRL+C. But it's not working (I followed Google's documentation BTW)
+        # try:
+        #     future.result()
+        # except KeyboardInterrupt:
+        #     print("Shutting down...")
+        #     future.cancel()
+
+        # Works for now
+        try:
+            logger.info("Listening for new messages...")
+            while True: 
+                continue
+        except KeyboardInterrupt:
+            logger.warning('Shutting down...')
+
+
+    GMAIL_SERVICE.users().stop(userId="me").execute()
     GMAIL_SERVICE.close()
     MONGO_DATABASE.client.close()
     CLOUD_STORAGE_CLIENT.close()
