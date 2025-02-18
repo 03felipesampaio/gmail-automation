ba0c53b246de29ba1379670d91dbb0355098ba0c
Felipe Amorim <03.felipesampaio@gmail.com>
2025-02-18 17:04:16 -0300

    Add action definition and database interaction for Gmail message processing

---
+++ src/actions/__init__.py
@@ -0,0 +1,2 @@
+from .default_actions import *
+from .define_action import actions
---
+++ src/actions/default_actions.py
@@ -0,0 +1,19 @@
+from .define_action import define_action
+from pathlib import Path
+import json
+
+
+@define_action(format="full")
+def save_to_json(path: str, message: dict, tet, exist_ok:bool|None=True) -> dict:
+    """Saves Gmail message to a JSON file.
+    
+    Args:
+        message (Message): Gmail message
+    """
+    dir_messages = Path(path)
+    dir_messages.mkdir(parents=True, exist_ok=True)
+    
+    with open(dir_messages / f"{message['id']}.json", "w") as f:
+        json.dump(message, f, ensure_ascii=False, indent=4)
+    
+    return message
