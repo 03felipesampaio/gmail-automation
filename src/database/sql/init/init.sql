70a6d46efdd7e95952a892cfda3252f009d8f723
Felipe Amorim <03.felipesampaio@gmail.com>
2025-02-18 17:04:23 -0300

    Update SQL initialization script to standardize timestamp with time zone and modify foreign key references

--- src/database/sql/init/init.sql
+++ src/database/sql/init/init.sql
@@ -4,8 +4,8 @@ DO $$ BEGIN
             "classifier_id" SERIAL NOT NULL,
             "classifier_name" VARCHAR(255) NOT NULL,
             "gmail_query" VARCHAR(255) NOT NULL,
-            "created_at" TIMESTAMP(0) WITH TIME zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            "updated_at" TIMESTAMP(0) WITH TIME zone
+            "created_at" TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "updated_at" TIMESTAMP(0) WITH TIME ZONE
         );
         ALTER TABLE "classifiers" ADD PRIMARY KEY("classifier_id");
     END IF;
@@ -17,8 +17,8 @@ DO $$ BEGIN
             "classifier_execution_id" UUID NOT NULL,
             "execution_id" UUID NOT NULL,
             "classifier_id" BIGINT NOT NULL,
-            "started_at" TIMESTAMP(0) WITH TIME zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            "finished_at" TIMESTAMP(0) WITH TIME zone,
+            "started_at" TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "finished_at" TIMESTAMP(0) WITH TIME ZONE,
             "status" VARCHAR(255) NOT NULL DEFAULT 'RUNNING'
         );
         ALTER TABLE "classifier_executions" ADD PRIMARY KEY("classifier_execution_id");
@@ -29,8 +29,8 @@ DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'executions') THEN
         CREATE TABLE "executions"(
             "execution_id" UUID NOT NULL,
-            "started_at" TIMESTAMP(0) WITH TIME zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            "finished_at" TIMESTAMP(0) WITH TIME zone,
+            "started_at" TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "finished_at" TIMESTAMP(0) WITH TIME ZONE,
             "status" VARCHAR(255) NOT NULL DEFAULT 'RUNNING'
         );
         ALTER TABLE "executions" ADD PRIMARY KEY("execution_id");
@@ -40,27 +40,28 @@ END $$;
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'action_templates') THEN
         CREATE TABLE "action_templates"(
-            "action_id" SERIAL NOT NULL,
             "action_name" VARCHAR(255) NOT NULL,
-            "created_at" TIMESTAMP(0) WITH TIME zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            "updated_at" TIMESTAMP(0) WITH TIME zone
+            "action_description" VARCHAR(255),
+            "format" VARCHAR(255) NOT NULL,
+            "created_at" TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "updated_at" TIMESTAMP(0) WITH TIME ZONE
         );
-        ALTER TABLE "action_templates" ADD PRIMARY KEY("action_id");
+        ALTER TABLE "action_templates" ADD PRIMARY KEY("action_name");
     END IF;
 END $$;
 
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'action_parameter_templates') THEN
         CREATE TABLE "action_parameter_templates"(
-            "action_id" BIGINT NOT NULL,
+            "action_name" VARCHAR(255) NOT NULL,
             "parameter_name" VARCHAR(255) NOT NULL,
             "parameter_type" VARCHAR(255) NOT NULL,
             "parameter_is_nullable" BOOLEAN NOT NULL,
             "parameter_default" VARCHAR(255) NOT NULL,
-            "created_at" TIMESTAMP(0) WITH TIME zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            "updated_at" TIMESTAMP(0) WITH TIME zone
+            "created_at" TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "updated_at" TIMESTAMP(0) WITH TIME ZONE
         );
-        ALTER TABLE "action_parameter_templates" ADD PRIMARY KEY("action_id", "parameter_name");
+        ALTER TABLE "action_parameter_templates" ADD PRIMARY KEY("action_name", "parameter_name");
     END IF;
 END $$;
 
@@ -69,7 +70,7 @@ DO $$ BEGIN
         CREATE TABLE "classifier_actions"(
             "classifier_action_id" SERIAL NOT NULL,
             "classifier_id" BIGINT NOT NULL,
-            "action_id" BIGINT NOT NULL,
+            "action_name" VARCHAR(255) NOT NULL,
             "parameters" JSON NOT NULL
         );
         ALTER TABLE "classifier_actions" ADD PRIMARY KEY("classifier_action_id");
@@ -81,8 +82,8 @@ DO $$ BEGIN
         CREATE TABLE "classifier_messages_execution"(
             "message_id" VARCHAR(255) NOT NULL,
             "classifier_execution_id" UUID NOT NULL,
-            "started_at" TIMESTAMP(0) WITH TIME zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            "finished_at" TIMESTAMP(0) WITH TIME zone,
+            "started_at" TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "finished_at" TIMESTAMP(0) WITH TIME ZONE,
             "status" VARCHAR(255) NOT NULL DEFAULT 'RUNNING'
         );
         ALTER TABLE "classifier_messages_execution" ADD PRIMARY KEY(
@@ -100,13 +101,13 @@ END $$;
 
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.table_constraints WHERE constraint_name = 'action_parameter_templates_action_id_foreign') THEN
-        ALTER TABLE "action_parameter_templates" ADD CONSTRAINT "action_parameter_templates_action_id_foreign" FOREIGN KEY("action_id") REFERENCES "action_templates"("action_id");
+        ALTER TABLE "action_parameter_templates" ADD CONSTRAINT "action_parameter_templates_action_id_foreign" FOREIGN KEY("action_name") REFERENCES "action_templates"("action_name");
     END IF;
 END $$;
 
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.table_constraints WHERE constraint_name = 'classifier_actions_action_id_foreign') THEN
-        ALTER TABLE "classifier_actions" ADD CONSTRAINT "classifier_actions_action_id_foreign" FOREIGN KEY("action_id") REFERENCES "action_templates"("action_id");
+        ALTER TABLE "classifier_actions" ADD CONSTRAINT "classifier_actions_action_id_foreign" FOREIGN KEY("action_name") REFERENCES "action_templates"("action_name");
     END IF;
 END $$;
 
